bda890cc6ef8b182d936bb84874ff57fde4b5b67
Nadine Ott <nadineott7@yahoo.com>
2016-08-25 11:37:02 -0700

    incorporate angular show + styled buttons

--- personalSiteController.js
+++ personalSiteController.js
@@ -1,7 +1,29 @@
 angular.module('personalSite', [])
 
+// show 1 = landing/picture
+//show 2 = alternative??
+// show 3 = blog
+// show 4 = resume
+// show 5 = recent projects
 
 .controller('personalSiteController', function($scope) {
   console.log("controller working");
-  $scope.show =1;
+  $scope.show = 1;
+
+  $scope.showHome = function(){
+    console.log("showHome function called")
+    $scope.show = 1;
+  }
+  $scope.showBlog = function(){
+    console.log("showBlog function called")
+    $scope.show = 3;
+  }
+  $scope.showResume = function(){
+    console.log("showResume function called")
+    $scope.show = 4;
+  }
+  $scope.showProjects = function(){
+    console.log("showProjects function called")
+    $scope.show = 5;
+  }
 });
